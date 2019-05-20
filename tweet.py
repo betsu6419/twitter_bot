695e6b476c329c68a9e95678f6c267f07e320e33
betsu6419 <yusawabe@gmail.com>
2019-05-21 01:27:29 +0900

    pass

--- tweet.py
+++ tweet.py
@@ -1,12 +1,14 @@
 import os
 import json
 import requests
+import datetime
 from requests_oauthlib import OAuth1Session
 
 
 twitter = OAuth1Session(os.environ["CONSUMER_KEY"],os.environ["CONSUMER_SECRET"],os.environ["ACCESS_KEY"],os.environ["ACCESS_SECRET"])
 url = "https://api.twitter.com/1.1/statuses/update.json"
-params = {'status':'test'}
+dt = datetime.datetime.now()
+params = {'status':"現在は{0:%Y%m%d}".format(dt)}
 res = twitter.post(url,params = params)
 timeline = json.loads(res.text)
 
