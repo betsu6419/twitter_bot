c71b0db2161c8c23b487b37fe09b0c10f0380b00
betsu6419 <yusawabe@gmail.com>
2019-05-21 01:12:38 +0900

    pass

--- tweet.py
+++ tweet.py
@@ -4,10 +4,7 @@ import requests
 from requests_oauthlib import OAuth1Session
 
 
-twitter = OAuth1Session(os.environ["CONSUMER_KEY"],
-                  os.environ["CONSUMER_SECRET"],
-                  os.environ["ACCESS_KEY"],
-                  os.environ["ACCESS_SECRET"])
+twitter = OAuth1Session(os.environ["CONSUMER_KEY"],os.environ["CONSUMER_SECRET"],os.environ["ACCESS_KEY"],os.environ["ACCESS_SECRET"])
 url = "https://api.twitter.com/1.1/statuses/update.json"
 params = {'status':'test'}
 res = twitter.post(url,params = params)
